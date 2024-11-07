cec8208e536ba5454633fa416e2dcd086c0d3b6f
A-nish-kr <146023446+A-nish-kr@users.noreply.github.com>
2024-11-07 23:50:06 +0530

    Update page.tsx

--- src/app/ForgotPassword/page.tsx
+++ src/app/ForgotPassword/page.tsx
@@ -10,51 +10,61 @@ export default function Forgotpassword(){
         oldPassword:"",
         newPassword: "",
     })
-    const [loading, setLoading] = React.useState(false);
+    const [loading, setLoading] = useState(false);
+
+    // Define a type for the error
+    interface AxiosError extends Error {
+        response?: {
+            data: any;
+        };
+    }
 
     const ChangePassword = async () => {
         try {
             setLoading(true);
             const response = await axios.post("/api/users/forgotpassword", {token, ...user});
-            console.log("passwordchanged Successfully", response.data);
-            router.push("/login");
-        } catch (error:any) {
+            console.log("password changed Successfully", response.data);
+            router.push("/Login");
+        } catch (error: AxiosError) {
             console.log("password change failed", error.message);
         } finally{
-        setLoading(false);
+            setLoading(false);
         }
     }
+
     useEffect(() => {
         const urlToken = window.location.search.split("=")[1];
         setToken(urlToken || "");
     }, []);
 
     return(
-<div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <h1>{loading ? "Processing" : "Login"}</h1>
-        <hr />
-        
-        <label htmlFor="OldPassword">Old Password</label>
-        <input 
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
-            id="OldPassword"
-            type="text"
-            value={user.oldPassword}
-            onChange={(e) => setUser({...user, oldPassword : e.target.value})}
-            placeholder="email"
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>{loading ? "Processing" : "Login"}</h1>
+            <hr />
+            
+            <label htmlFor="OldPassword">Old Password</label>
+            <input 
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+                id="OldPassword"
+                type="text"
+                value={user.oldPassword}
+                onChange={(e) => setUser({...user, oldPassword : e.target.value})}
+                placeholder="email"
             />
-        <label htmlFor="password">password</label>
-        <input 
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
-            id="password"
-            type="password"
-            value={user.newPassword}
-            onChange={(e) => setUser({...user, newPassword: e.target.value})}
-            placeholder="password"
+            <label htmlFor="password">password</label>
+            <input 
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+                id="password"
+                type="password"
+                value={user.newPassword}
+                onChange={(e) => setUser({...user, newPassword: e.target.value})}
+                placeholder="password"
             />
-          <button
-            onClick={ChangePassword}
-            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">Change Password</button>
+            <button
+                onClick={ChangePassword}
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">
+                Change Password
+            </button>
         </div>
     )
 }
