f74a8e4fb2b58f0a8e5e7b8b08e2232e8fa79b22
A-nish-kr <146023446+A-nish-kr@users.noreply.github.com>
2024-11-08 00:11:09 +0530

    Update page.tsx

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -68,7 +68,7 @@ export default function LoginPage() {
             onClick={onLogin}
             disabled = {buttonDisabled}
             className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">Login here</button>
-            <Link href="/signup">Visit Signup page</Link>
+            <Link href="/Signup">Visit Signup page</Link>
         </div>
     )
 
